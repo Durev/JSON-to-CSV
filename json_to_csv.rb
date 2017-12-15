aa191511c6e4fc5f968f7c9d235eca88eaabe8d1
Durev <vincent.durewski@gmail.com>
2017-12-15 13:28:55 +0100

    Add recursivity, support nested hashes to get fields values

--- json_to_csv.rb
+++ json_to_csv.rb
@@ -4,16 +4,12 @@ require 'csv'
 file = File.open("users.json").read
 json = JSON.parse(file)
 
-# write field names first
+# Write field names first
 # In this case, we assume that all objects are normalized
 
 head = json.first.collect {|key, value| key}
 
-def flatten_hash_keys(nested_hashs)
-  #
-end
-
-# then write fields
+# Then write fields
 
 def hash_to_array(hash)
   hash.collect do |key, value|
@@ -23,14 +19,13 @@ def hash_to_array(hash)
     when Array
       value.join(",")
     when Hash
-      # use recursivity until there aren't anymore nested hashes
+      hash_to_array(value)
     end
   end
+  .flatten
 end
 
-p hash_to_array(json.first)
-
-# Write in CSV file :
+# Write in CSV file
 
 CSV.open("users.csv", "wb") do |csv|
   csv << head
