0fd8e7f8ab4f71fe5e6e1d78fea3d0f5f6c85305
Durev <vincent.durewski@gmail.com>
2017-12-15 22:17:11 +0100

    Complete fields names, now supporting nested hashes

--- json_to_csv.rb
+++ json_to_csv.rb
@@ -4,13 +4,23 @@ require 'csv'
 file = File.open("users.json").read
 json = JSON.parse(file)
 
-# Write field names first
-# In this case, we assume that all objects are normalized
+# Get field names first - In this case, we assume that all objects are normalized
+def get_fields_name(object, path = nil)
+  case object
+  when Hash
+    object.collect do |key, value|
+      if path
+        get_fields_name(value, "#{path}.#{key}")
+      else
+        get_fields_name(value, "#{key}")
+      end
+    end.flatten
+  else
+    path
+  end
+end
 
-head = json.first.collect {|key, value| key}
-
-# Then write fields
-
+# Then get fields content
 def hash_to_array(hash)
   hash.collect do |key, value|
     case value
@@ -21,13 +31,11 @@ def hash_to_array(hash)
     when Hash
       hash_to_array(value)
     end
-  end
-  .flatten
+  end.flatten
 end
 
 # Write in CSV file
-
 CSV.open("users.csv", "wb") do |csv|
-  csv << head
+  csv << get_fields_name(json.first)
   json.each {|object| csv << hash_to_array(object)}
 end
